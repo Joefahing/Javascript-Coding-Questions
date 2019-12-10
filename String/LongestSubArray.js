f9e1febc1b1c47e3623c7c72755846d783bd1f91
Joefa Cao <cao11d@uwindsor.ca>
2019-12-10 14:08:38 -0500

    resync

---
+++ String/LongestSubArray.js
@@ -0,0 +1,12 @@
+
+const LongestSubarray = (array, k) => {
+    let max = Number.NEGATIVE_INFINITY;
+    let sum = 0;
+    let left = right = 0;
+
+
+    for(let i = 0; i < array.length; i++){
+        
+    }
+
+}
